e743087412aa5d492ef5285d1432619f77117669
Rubens Algeri <rubens_zys@hotmail.com>
2022-09-24 14:26:43 -0300

    fix: fixed input pattern not working

--- src/components/TelaCadastro.js
+++ src/components/TelaCadastro.js
@@ -5,81 +5,105 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function TelaCadastro() {
-    const [cadastro, setCadastro] = useState({
-        email: "",
-        password: "",
-        name: "",
-        confirmPassword: "",
-    });
-    const { email, password, name, confirmPassword } = cadastro;
+  const [cadastro, setCadastro] = useState({
+    email: "",
+    password: "",
+    name: "",
+    confirmPassword: "",
+  });
+  const { email, password, name, confirmPassword } = cadastro;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function cadastrar(event) {
-        event.preventDefault();
-        const promessa = axios.post(
-            "https://projeto-13-mywallet.herokuapp.com/sign-up",
-            { email, password, username: name }
-        );
-        promessa.then(() => {
-            navigate("/");
-        });
-        promessa.catch((err) => {
-            alert(`deu ruim, ${err.message}`);
-        });
-    }
-
-    return (
-        <Cadastro>
-            <h1>MyWallet</h1>
-            <form onSubmit={cadastrar}>
-
-                <input
-                    type="text"
-                    placeholder="Nome"
-                    value={cadastro.name}
-                    onChange={(e) =>
-                        setCadastro({ name: e.target.value, email, password, confirmPassword })
-                    }
-                    required
-                ></input>
-                <input
-                    type="email"
-                    placeholder="E-mail"
-                    value={cadastro.email}
-                    onChange={(e) =>
-                        setCadastro({ email: e.target.value, password, name, confirmPassword })
-                    }
-                    required
-                ></input>
-                <input
-                    type="password"
-                    placeholder="Senha"
-                    value={cadastro.password}
-                    onChange={(e) =>
-                        setCadastro({ password: e.target.value, email, name, confirmPassword })
-                    }
-                    required
-                ></input>
-                <input
-                    type="password"
-                    placeholder="Confirme a senha"
-                    value={cadastro.confirmPassword}
-                    onChange={(e) =>
-                        setCadastro({ confirmPassword: e.target.value, email, password, name })
-                    }
-                    required
-                ></input>
-                {password === confirmPassword ? (
-                    <button type="submit">Cadastrar</button>
-                ) : (
-                    <button disabled type="submit">Cadastrar</button>
-                )}
-
-            </form>
-            <Link to="/">Já tem uma conta? Faça login!</Link>
-        </Cadastro>
+  function cadastrar(event) {
+    event.preventDefault();
+    const promessa = axios.post(
+      "https://projeto-13-mywallet.herokuapp.com/sign-up",
+      {
+        email,
+        password,
+        username: name,
+      }
     );
+    promessa.then(() => {
+      navigate("/");
+    });
+    promessa.catch((err) => {
+      alert(`deu ruim, ${err.message}`);
+    });
+  }
+
+  return (
+    <Cadastro>
+      <h1>MyWallet</h1>
+      <form onSubmit={cadastrar}>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={cadastro.name}
+          onChange={(e) =>
+            setCadastro({
+              name: e.target.value,
+              email,
+              password,
+              confirmPassword,
+            })
+          }
+          required
+        ></input>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={cadastro.email}
+          onChange={(e) =>
+            setCadastro({
+              email: e.target.value,
+              password,
+              name,
+              confirmPassword,
+            })
+          }
+          required
+        ></input>
+        <input
+          type="password"
+          placeholder="Senha"
+          value={cadastro.password}
+          onChange={(e) =>
+            setCadastro({
+              password: e.target.value,
+              email,
+              name,
+              confirmPassword,
+            })
+          }
+          required
+        ></input>
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          value={cadastro.confirmPassword}
+          onChange={(e) =>
+            setCadastro({
+              confirmPassword: e.target.value,
+              email,
+              password,
+              name,
+            })
+          }
+          required
+        ></input>
+        {password === confirmPassword ? (
+          <button type="submit">Cadastrar</button>
+        ) : (
+          <button disabled type="submit">
+            Cadastrar
+          </button>
+        )}
+      </form>
+      <Link to="/">Já tem uma conta? Faça login!</Link>
+    </Cadastro>
+  );
 }
 const Cadastro = styled.div`
   display: flex;
@@ -89,11 +113,11 @@ const Cadastro = styled.div`
   width: 100vw;
   h1 {
     margin: 95px 0 28px 0;
-    font-family: 'Saira Stencil One', cursive;
+    font-family: "Saira Stencil One", cursive;
     font-size: 32px;
     line-height: 50px;
 
-    color: #FFFFFF;
+    color: #ffffff;
   }
   form {
     display: flex;
